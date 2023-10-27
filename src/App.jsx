e4ce30a03fb91c82834014932f2c8fe2668a8156
Dalim017 <mostafazur038695@gmail.c0m>
2023-10-27 21:46:28 +0200

    created the product page, cartPage, orderPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import HomePage from "./Components/Home/HomePage";
+import ProductsPage from "./Components/Products/ProductsPage";
+import SingleProductPage from "./Components/SingleProduct/SingleProductPage";
+import CartPage from "./Components/Cart/CartPage";
+import MyOrderPage from "./Components/MyOrder/MyOrderPage";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
       <main>
-        <HomePage />
+        {/* <HomePage /> */}
+        {/* <ProductsPage /> */}
+        {/* <SingleProductPage /> */}
+        {/* <CartPage /> */}
+        <MyOrderPage />
       </main>
     </div>
   );
